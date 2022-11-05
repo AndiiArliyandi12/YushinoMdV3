6bece2e33a50819bc0118176e9f4584a1adc8a3e
AndiiArliyandi12 <87647174+AndiiArliyandi12@users.noreply.github.com>
2022-11-05 10:47:10 +0700

    Update g-add.js

--- plugins/g-add.js
+++ plugins/g-add.js
@@ -79,14 +79,15 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
 handler.help = ['add', '+'].map(v => v + ' nomor,nomor')
 handler.tags = ['group']
 handler.command = /^(add|\+|sendlink)$/i
-handler.owner = false
+handler.owner = true
+
 handler.mods = false
 handler.premium = false
 handler.group = true
 handler.private = false
 
-handler.admin = true
 handler.botAdmin = true
+handler.owner = true
 
 handler.fail = null
 handler.limit = true
