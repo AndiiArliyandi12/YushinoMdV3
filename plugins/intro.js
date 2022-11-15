816c676b2f41930da77ece06d3e8e6cf6f452a44
AndiiArliyandi12 <87647174+AndiiArliyandi12@users.noreply.github.com>
2022-11-15 10:54:06 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -3,13 +3,13 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let krtu = `☰⃟⟣⟔ Yᴏᴜʀ Cᴀʀᴅ Iɴᴛʀᴏ ⟓⟢⃟
-┇⃟⟣⟜ *Nama:* 
-┇⃟⟣⟜ *Umur:* 
-┇⃟⟣⟜ *Alamat:*
-┇⃟⟣⟜ *Hobi:*
-┇⃟⟣⟜ *Pasangan:*
-┖┈┈┈╼╸╸╸╴╴╴ ╳
+let krtu = `┌–––––––––––––––––✥
+│「▰▰▰▱▱▱▱▱▱▱」
+└┬❖ 「 Kartu Intro 」
+┌┤❀  ɴᴀᴍᴀ :
+┊│❀  ᴋᴏᴛᴀ : 
+┊│❀  ᴡᴀʀɴᴀ ʙʜ ʏᴀɴɢ ᴅɪ ᴘᴀᴋᴀɪ :
+│└────────────┈
 `
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
